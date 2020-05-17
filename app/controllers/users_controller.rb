18cde1a60f30875ac3291cd126f1d5110e90727b
SuelyBarreto <suely@barreto.us>
2020-05-17 14:56:40 -0700

    Finished Woks Controller. Starting User.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+
+  def login_form
+    @user = User.new
+  end
+
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   resources :works
   get "/works/:id/upvote", to: "works#upvote", as: :upvote_work
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end

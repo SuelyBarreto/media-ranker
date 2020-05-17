9db88fc1d38ddf9f99f30259e4f68567428063be
SuelyBarreto <suely@barreto.us>
2020-05-17 15:36:34 -0700

    Login and Logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,28 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def login
+    name = params[:user][:name]
+    user = User.find_by(name: name)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{name}"
+    else
+      user = User.create(name: name, joined: Date.today)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{name}"
+    end
   
+    redirect_to root_path
+    return
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+  
+    redirect_to root_path
+    return
+  end
 
 end

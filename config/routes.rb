7d289195aea36a213f6a641852c23f5ed766d7a9
SuelyBarreto <suely@barreto.us>
2020-05-17 19:41:08 -0700

    User Controller Finished

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :works
   get "/works/:id/upvote", to: "works#upvote", as: :upvote_work
 
+  get "/users", to: "users#index", as: :users
+  get "/users/:id", to: "users#show", as: :user
   get "/login", to: "users#login_form", as: "login"
   post "/login", to: "users#login"
   post "/logout", to: "users#logout", as: "logout"
-  get "/users/current", to: "users#current", as: "current_user"
 end

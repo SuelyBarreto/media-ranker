7ceaf524a9444dfa7a185368ab4d5617412f31ef
SuelyBarreto <suely@barreto.us>
2020-05-20 19:13:28 -0700

    Removed unnecessary methods from models, after creating Vote model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,7 @@ class User < ApplicationRecord
   has_many :works, through: :votes
   
   validates :name, uniqueness: true, presence: true
-
-  def votes
-    return Vote.user_votes(self)
-  end
-  
+ 
   def voted?(work)
     return Vote.voted?(work, self)
   end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,23 +2,7 @@ class Vote < ApplicationRecord
 
   belongs_to :user, counter_cache: true
   belongs_to :work, counter_cache: true
-
-  def self.user_votes(user)
-    return Vote.where(user_id: user.id).size
-  end
-  
-  def self.works(user)
-    return Vote.where(user_id: user.id)
-  end
-
-  def self.work_votes(work)
-    return Vote.where(work_id: work.id).size
-  end
-  
-  def self.users(work)
-    return Vote.where(work_id: work.id)
-  end
-  
+   
   def self.voted?(work, user)
     return Vote.where(work_id: work.id, user_id: user.id).size > 0
   end
